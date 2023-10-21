7845d5a7dc678c4953670dae1baad1d444472a4e
elias <eliaslaramartinez@gmail.com>
2023-10-21 19:58:48 -0300

    entendiendo la app

---
+++ Leccion 21/js/app.js
@@ -0,0 +1,17 @@
+const ingresos = [
+    new Ingreso('Salario',21.000),
+    new Ingreso('Venta Coche',14.600)
+];
+
+const egresos = [
+    new Egreso('Renta departamento',900),
+    new Egreso('Ropa',400)
+];
+
+let cargarApp = ()=>{
+    cargarCabecero();
+}
+
+let cargarCabecero = () =>{
+    
+}
