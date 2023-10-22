f89448fa65a7913187961936fb33c79c32ef3366
elias <eliaslaramartinez@gmail.com>
2023-10-21 22:33:09 -0300

    app de presupuesto

--- Leccion 21/js/app.js
+++ Leccion 21/js/app.js
@@ -1,10 +1,10 @@
 const ingresos = [
-    new Ingreso('Salario',21.000),
-    new Ingreso('Venta Coche',14.600)
+    new Ingreso('Salario',23000),
+    new Ingreso('Venta Coche',50000)
 ];
 
 const egresos = [
-    new Egreso('Renta departamento',900),
+    new Egreso('Renta departamento',500),
     new Egreso('Ropa',400)
 ];
 
@@ -12,6 +12,35 @@ let cargarApp = ()=>{
     cargarCabecero();
 }
 
+let totalIngresos = ()=>{
+    let totalIngreso = 0;
+    for(let ingreso of ingresos){
+        totalIngreso += ingreso.valor;
+    }
+    return totalIngreso;
+}
+
+let totalEgresos = ()=>{
+    let totalEgreso = 0;
+    for(let egreso of egresos){
+        totalEgreso += egreso.valor;
+    }
+    return totalEgreso;
+}
+
 let cargarCabecero = () =>{
-    
+    let presupuesto = totalIngresos()- totalEgresos();
+    let porcentajeEgreso = totalEgresos()/totalIngresos();
+    document.getElementById('presupuesto').innerHTML = formatoMoneda(presupuesto);
+    document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgreso);
+    document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
+    document.getElementById('egresos').innerHTML = formatoMoneda(totalEgresos());
+}
+
+const formatoMoneda = (valor)=>{
+   return  valor.toLocaleString('es-CL',{style:'currency',currency:'CLP',minimumFractionDigits:2});
+}
+
+const formatoPorcentaje = (valor)=>{
+    return valor.toLocaleString('es-CL',{style:'percent',minimumFractionDigits:2});
 }
