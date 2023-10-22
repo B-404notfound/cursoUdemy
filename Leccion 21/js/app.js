43f1cf248bf8ebfcfe447ce641a55ab270298c81
elias <eliaslaramartinez@gmail.com>
2023-10-22 15:07:05 -0300

    boton eliminar

--- Leccion 21/js/app.js
+++ Leccion 21/js/app.js
@@ -65,13 +65,18 @@ const crearIngresoHTML = (ingreso)=>{
         <div class="elemento_valor">+ ${formatoMoneda(ingreso.valor)}</div>
         <div class="elemento_eliminar">
             <button class="elemento_eliminar--btn">
-                <ion-icon name="close-circle-outline"></ion-icon>
+                <ion-icon name="close-circle-outline"
+             onclick='eliminarIngreso(${ingreso.id})'></ion-icon>
             </button>
         </div>
     </div>
 </div>
     `;
     return ingresoHTML;
+}
+
+const eliminarIngreso = (id)=>{
+   let indiceEliminar =  ingresos.findIndex(ingreso => ingreso.id===id);
 }
 
 const cargarEgresos = ()=>{
