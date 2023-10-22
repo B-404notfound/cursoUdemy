5f5560ab6f0fdc7228d789ebd18cc6fbe4a15f06
elias <eliaslaramartinez@gmail.com>
2023-10-22 17:10:31 -0300

    app terminada

--- Leccion 21/js/app.js
+++ Leccion 21/js/app.js
@@ -77,6 +77,9 @@ const crearIngresoHTML = (ingreso)=>{
 
 const eliminarIngreso = (id)=>{
    let indiceEliminar =  ingresos.findIndex(ingreso => ingreso.id===id);
+   ingresos.splice(indiceEliminar,1);
+   cargarCabecero();
+   cargarIngresos();
 }
 
 const cargarEgresos = ()=>{
@@ -85,6 +88,7 @@ const cargarEgresos = ()=>{
         egresosHTML += crearEgresoHTML(egreso);
     }
     document.getElementById('lista-egresos').innerHTML = egresosHTML;
+    
 }
 
 const crearEgresoHTML = (egreso)=>{
@@ -96,11 +100,38 @@ const crearEgresoHTML = (egreso)=>{
         <div class="elemento_porcentaje">${formatoPorcentaje(egreso.valor/totalEgresos())}</div>
         <div class="elemento_eliminar">
             <button class="elemento_eliminar--btn">
-                <ion-icon name="close-circle-outline"></ion-icon>
+                <ion-icon name="close-circle-outline"
+                onclick='eliminarEgreso(${egreso.id})'></ion-icon>
             </button>
         </div>
     </div>
 </div>
     `;
     return egresoHTML;
+}
+
+const eliminarEgreso = (id)=>{
+    let indiceEliminar = egresos.findIndex(egreso => egreso.id===id);
+    egresos.splice(indiceEliminar,1);
+    cargarCabecero();
+    cargarEgresos();
+}
+
+let agregarDato = ()=>{
+    let forma = document.forms['forma'];
+    let tipo = forma['tipo'];
+    let descripcion = forma['descripcion'];
+    let valor = forma['valor'];
+    if(descripcion.value !==''&& valor.value !=='' ){
+        if(tipo.value ==='ingreso'){
+            ingresos.push(new Ingreso(descripcion.value,+valor.value));
+            cargarCabecero();
+            cargarIngresos();
+        }
+        else if(tipo.value==='egreso'){
+            egresos.push(new Egreso(descripcion.value,+valor.value));
+            cargarCabecero();
+            cargarEgresos();
+        }
+    }
 }
